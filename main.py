d251f0d347cb04ff1c5d2f5ad5d81ce5fb01612b
Alex Rozenshteyn <rpglover64@gmail.com>
2024-06-29 20:07:32 -0400

    Complete second iteration

--- main.py
+++ main.py
@@ -2,42 +2,63 @@ import torchvision
 import torch
 from torchvision import transforms
 from PIL import Image
-from pathlib import Path
+from dataclasses import dataclass
 
 model = torchvision.models.inception_v3(weights=torchvision.models.Inception_V3_Weights.DEFAULT)
 model.eval()
-img = Image.open("input.jpg")
 
-preprocess = transforms.Compose([
-    transforms.Resize(299),
-    transforms.CenterCrop(299),
-    transforms.ToTensor(),
-])
+def load_image(path:str) -> torch.Tensor:
+    img = Image.open(path)
 
-image_tensor = preprocess(img)
+    return transforms.Compose([
+        transforms.Resize(299),
+        transforms.ToTensor(),
+    ])(img)
 
-noise = torch.zeros_like(image_tensor, requires_grad=True)
+image_tensor = load_image("input.jpg")
 
-opt = torch.optim.SGD([noise], weight_decay=0.1)
-loss_fn = torch.nn.CrossEntropyLoss()
+@dataclass
+class Params:
+    target: int
+    learning_rate: float
+    weight_decay: float
+    num_iter: int
 
-target_class = 0
-with torch.no_grad():
-    out = model(image_tensor.unsqueeze(0))[0]
-    loss = loss_fn(out, torch.tensor(target_class))
-    print(loss)
 
-n_iter=100
-for i in range(n_iter):
-    perturbed = image_tensor + noise
-    out = model(perturbed.unsqueeze(0))[0]
-    loss = loss_fn(out, torch.tensor(target_class))
-    print("iter", i, loss)
-    loss.backward()
-    opt.step()
+default_params = Params(0, 1e-2, 100, 200)
+
+def train(img:torch.Tensor, params:Params) -> torch.Tensor:
+
+    noise = torch.zeros_like(img, requires_grad=True)
+
+    opt = torch.optim.SGD([noise], weight_decay=params.weight_decay, lr=params.learning_rate)
+    loss_fn = torch.nn.CrossEntropyLoss()
+
+    with torch.no_grad():
+        out = model(img.unsqueeze(0))[0]
+        loss = loss_fn(out, torch.tensor(params.target))
+        print(loss)
+
+    for i in range(params.num_iter):
+        perturbed = img + noise
+        out = model(perturbed.unsqueeze(0))[0]
+        loss = loss_fn(out, torch.tensor(params.target))
+        print("iter", i, loss)
+        loss.backward()
+        opt.step()
     
-with torch.no_grad():
-    perturbed = image_tensor + noise
-    out = model(perturbed.unsqueeze(0))[0]
-    loss = loss_fn(out, torch.tensor(target_class))
-    print(loss)
+    return noise
+    
+def eval(img:torch.Tensor, noise:torch.tensor, params:Params):
+    with torch.no_grad():
+        out_orig = model(img.unsqueeze(0))[0]
+        perturbed = img + noise
+        out_perturbed = model(perturbed.unsqueeze(0))[0]
+        prob_orig = torch.nn.functional.softmax(out_orig, dim=0)
+        prob_perturbed = torch.nn.functional.softmax(out_perturbed, dim=0)
+        top_orig = torch.topk(prob_orig, 5)
+        top_perturbed = torch.topk(prob_perturbed, 5)
+        print(top_orig)
+        print(top_perturbed)
+        print(prob_perturbed[params.target])
+        print(prob_perturbed[top_orig.indices])
--- notes.md
+++ notes.md
@@ -22,3 +22,30 @@ Next attempt:
 
 * parameters rather than hard code most things
 * Experiment with stronger regularization, more training iterations, and lower initial learning rate
+
+## V2
+
+* Large learning rate with strong regularization seems to give an invisible result
+* The target class becomes most likely, but not 99% likely
+
+Noise after optimization:
+
+![noise after optimization](<V2 artifacts/noise.png>)
+
+Image with noise:
+
+![image after noise addition](<V2 artifacts/perturbed.png>)
+
+If I spent more time:
+
+* I kept the debug printing in; I would remove it
+* I would consider using a standard training loop rather than an ad-hoc one
+* I would look for ways to avoid the noise becoming visible
+  * It's mostly in dark areas of the picture; this leads to two immediate directions of exploration:
+    * Avoid adding any noise to the dark sections of the image
+    * Use multiplicative rather than additive noise
+  * Add a regularization term that penalizes based on perceptual distance rather than just noise magnitude
+  * Use a different parameterization of noise, such as adding it to the Fourier domain
+* The ideal number of iterations depends on the image and target class; it shouldn't be too hard to find a better stopping criterion
+* SGD with a fixed step size is definitely not the best optimizer for this; at least, I'd explore using line search
+* Testing: For multiple starting images and multiple target classes, run the adversarial training; each one should successfully fool the classifier and remain invisible
